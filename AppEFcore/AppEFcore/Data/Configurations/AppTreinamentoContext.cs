4f45f37cb4ead9a36608e640cf0d790bb3c67401
Jeffconexion <jeff.debian2016@gmail.com>
2021-05-05 16:37:20 -0300

    Introdução ao EFcore finalizado.

--- AppEFcore/AppEFcore/Data/Configurations/AppTreinamentoContext.cs
+++ AppEFcore/AppEFcore/Data/Configurations/AppTreinamentoContext.cs
@@ -1,5 +1,8 @@
 ﻿using AppEFcore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 
 namespace AppEFcore.Data.Configurations
 {
@@ -9,20 +12,53 @@ namespace AppEFcore.Data.Configurations
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
 
+        //Para analisar resultados de consulta e outros procedimentos.
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=10.10.1.15;Database=DBEFcore;Trusted_Connection=True;");
-               
+            //optionsBuilder.UseSqlServer("Server=DESKTOP-EG5GF5V\\SQLEXPRESS;Database=DBEFcore;Trusted_Connection=True;");
+            //EnableRetryOnFailure: configuração para falhas ao tentar conectar ao banco de dados.
+            
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server=DESKTOP-EG5GF5V\\SQLEXPRESS;Database=DBEFcore;Trusted_Connection=True;",
+                 p => p.EnableRetryOnFailure(
+                     maxRetryCount: 2,
+                     maxRetryDelay: TimeSpan.FromSeconds(5),
+                     errorNumbersToAdd: null).MigrationsHistoryTable("curso_ef_core"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
-            // modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            //Forma tradicional: modelBuilder.ApplyConfiguration(new ClienteConfiguration());
 
             //Vai procurar no meu assemble pelo Fluent Api.
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppTreinamentoContext).Assembly);
+
+            //Atribuir varchar nas propriedades.
+            //MapearPropriedadesEsquecidas(modelBuilder);
         }
 
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    if (string.IsNullOrEmpty(property.GetColumnType())
+                        && !property.GetMaxLength().HasValue)
+                    {
+                        //property.SetMaxLength(100);
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
+        }
     }
 }
--- AppEFcore/AppEFcore/Program.cs
+++ AppEFcore/AppEFcore/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AppEFcore.Data.Configurations;
+using AppEFcore.Domain;
+using AppEFcore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AppEFcore
 {
@@ -6,7 +12,205 @@ namespace AppEFcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Faça a chamado do método.
         }
+
+        /// <summary>
+        /// 3 Maneiras para remover um dado.
+        /// </summary>
+        private static void RemoverRegistro()
+        {
+            using var db = new AppTreinamentoContext();
+
+            var cliente = db.Clientes.Find(2);
+            //var cliente = new Cliente { Id = 3 };
+            db.Clientes.Remove(cliente);
+            //db.Remove(cliente);
+            //db.Entry(cliente).State = EntityState.Deleted;
+
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Atualizar um dado.
+        /// </summary>
+        private static void AtualizarDados()
+        {
+            using var db = new AppTreinamentoContext();
+            var cliente = db.Clientes.Find(1);
+
+            cliente.Nome = "Marcos Silva";
+
+
+            //var cliente = new Cliente
+            //{
+            //    Id = 1
+            //};
+
+            //var clienteDesconectado = new
+            //{
+            //    Nome = "Cliente Desconectado Passo 3",
+            //    Telefone = "7966669999"
+            //};
+
+            //db.Attach(cliente);
+            //db.Entry(cliente).CurrentValues.SetValues(clienteDesconectado);
+
+            /* Não colocar o update permite somente atualização onde foi alterado(nome)
+             * db.Clientes.Update(cliente);
+             */
+
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Realizando consulta por método.
+        /// </summary>
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new AppTreinamentoContext();
+            var pedidos = db
+                .Pedidos
+                .Include(p => p.Itens)
+                    .ThenInclude(p => p.Produto) //Uso o ThenInclude para uma consulta secundária.
+                .ToList();
+
+            Console.WriteLine(pedidos.Count);
+        }
+
+        /// <summary>
+        /// 2 Maneiras de consultar dados. Por sintaxe e por método.
+        /// </summary>
+        private static void ConsultarDados()
+        {
+            using var db = new AppTreinamentoContext();
+
+            //Consulta Por sintaxe.
+            //var consultaPorSintaxe = (from c in db.Clientes where c.Id>0 select c).ToList();
+
+            //Consulta por Método.
+            var consultaPorMetodo = db.Clientes
+                .Where(p => p.Id > 0)
+                .OrderBy(p => p.Id)
+                .ToList();
+
+            foreach (var cliente in consultaPorMetodo)
+            {
+                Console.WriteLine($"Consultando Cliente: {cliente.Id}");
+                //db.Clientes.Find(cliente.Id);
+                db.Clientes.FirstOrDefault(p => p.Id == cliente.Id);
+            }
+        }
+
+        /// <summary>
+        /// 3 Maneiras para adicionar em lote.
+        /// </summary>
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste3",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Rafael Almeida",
+                Cep = "99999000",
+                Cidade = "Itabaiana",
+                Estado = "SE",
+                Telefone = "99000001111",
+            };
+
+            //var listaClientes = new[]
+            //{
+            //    new Cliente
+            //    {
+            //        Nome = "Teste 1",
+            //        Cep = "99999000",
+            //        Cidade = "Itabaiana",
+            //        Estado = "SE",
+            //        Telefone = "99000001115",
+            //    },
+            //    new Cliente
+            //    {
+            //        Nome = "Teste 2",
+            //        Cep = "99999000",
+            //        Cidade = "Itabaiana",
+            //        Estado = "SE",
+            //        Telefone = "99000001116",
+            //    },
+            //};
+
+
+            using var db = new AppTreinamentoContext();
+            db.AddRange(produto, cliente);
+            //db.Set<Cliente>().AddRange(listaClientes);
+            //db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
+
+        /// <summary>
+        /// 4 Maneiras para está adicionando em uma base de dados.
+        /// </summary>
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new AppTreinamentoContext();
+
+            db.Produtos.Add(produto); //Adicionando registro por meio da propriedade.(INDICADO)
+            db.Set<Produto>().Add(produto); //Adicionando registro de forma genérica.(INDICADO)
+            db.Entry(produto).State = EntityState.Added; //Forçando rastreamento de uma entidade.
+            db.Add(produto);//Adicionando pelo nosso context, ira ser feito uma exploração para saber qual entidade.
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
+
+        private static void CadastrarPedido()
+        {
+            using var db = new AppTreinamentoContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Obeservacao = "Pedido Teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                 {
+                     new PedidoItem
+                     {
+                         ProdutoId = produto.Id,
+                         Desconto = 0,
+                         Quantidade = 1,
+                         Valor = 10,
+                     }
+                 }
+            };
+
+            db.Pedidos.Add(pedido);
+
+            db.SaveChanges();
+        }
+
     }
 }
